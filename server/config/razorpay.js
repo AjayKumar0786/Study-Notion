9e73b2b10f8c9f2d802c9147bc4f2e3f40696151
Ajay Kumar <126579063+AjayKumar0786@users.noreply.github.com>
2024-08-18 02:14:51 +0530

    Update razorpay.js

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,5 +1,8 @@
 const Razorpay = require("razorpay");
 
+const key_id = "rzp_test_fUIZAI25WMgGwi"
+
+const key_secret = "o7TBIxOogcPsNQa9pTzsAoYf"
 
 exports.instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY,
